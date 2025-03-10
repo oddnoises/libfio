2621345f1d965bb599381e05659b77516ea43963
oddnoises <choob1900@mail.ru>
2025-03-10 20:13:53 +0300

    Add data transder func

--- src/fio.c
+++ src/fio.c
@@ -1,10 +1,40 @@
 #include <dirent.h>
 #include <errno.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <string.h>
 #include <lua5.4/lauxlib.h>
 #include <lua5.4/lua.h>
 #include <lua5.4/lualib.h>
+
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct Proc {
+  lua_State *L;
+  pthread_t thread;
+  pthread_cond_t cond;
+  const char *channel;
+  struct Proc *prev, *next;
+} Proc;
+
+/* Return Proc struct related to the lua_State */
+static Proc *getself(lua_State *L)
+{
+  Proc *p;
+  lua_getfield(L, LUA_REGISTRYINDEX, "_SELF");
+  p = (Proc *) lua_touserdata(L,-1);
+  lua_pop(L,1);
+  return p;
+}
+
+/* Send data between states */
+static void move_values(lua_State *send, lua_State *recv)
+{
+  int n = lua_gettop(send);
+  for (int i = 2; i <= n; i++) {
+    lua_pushstring(recv, lua_tostring(send, i));
+  }
+}
 
 static int l_dir(lua_State *L)
 {
