fce77811c09c1e97419e69941e7546228c78a385
oddnoises <choob1900@mail.ru>
2025-03-15 19:01:07 +0300

    Full join() and detach() implementation

--- src/fio.c
+++ src/fio.c
@@ -1,3 +1,4 @@
+//#include <bits/pthreadtypes.h>
 #include <dirent.h>
 #include <errno.h>
 #include <pthread.h>
@@ -7,6 +8,15 @@
 #include <lua5.4/lua.h>
 #include <lua5.4/lualib.h>
 
+static void *fio_thread(void *arg);
+static int fio_start(lua_State *L);
+static int fio_join(lua_State *L);
+static int fio_detach(lua_State *L);
+static int fio_mutex_create(lua_State *L);
+static void fio_mutex_lock();
+static void fio_mutex_unlock();
+static int fio_exit();
+static int l_dir(lua_State *L);
 int luaopen_fio(lua_State *L);
 
 //static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -16,6 +26,30 @@ typedef struct Proc {
   pthread_t thread;
 } Proc;
 
+typedef struct Mtex {
+  pthread_mutex_t *mtex;
+  char *name;
+  int users;
+} Mtex;
+/*-------------------------------------------------------------*/
+static const struct luaL_Reg fio [] = {
+  {"start", fio_start},
+  {"exit", fio_exit},
+  {"join", fio_join},
+  {"detach", fio_detach},
+  {"dir", l_dir},
+  {NULL, NULL}
+};
+/*-------------------------------------------------------------*/
+int luaopen_fio(lua_State *L)
+{
+  Proc *self = (Proc *) lua_newuserdata(L, sizeof(Proc));
+  lua_setfield(L, LUA_REGISTRYINDEX, "_SELF");
+  self->L = L;
+  self->thread = pthread_self();
+  luaL_newlib(L, fio);
+  return 1;
+}
 /*-------------------------------------------------------------*/
 static void *fio_thread(void *arg)
 {
@@ -29,20 +63,52 @@ static void *fio_thread(void *arg)
   return NULL;
 }
 /*-------------------------------------------------------------*/
+/* Запускает новый поток и возвращает его идентификатор */
 static int fio_start(lua_State *L)
 {
   pthread_t thread;
   const char *fname = luaL_checkstring(L, 1);
   lua_State *L1 = luaL_newstate();
   if (L1 == NULL) luaL_error(L, "Can't create new Lua state!");
-  if (luaL_loadfile(L1, fname))
+  if (luaL_loadfile(L1, fname)) // Возможно, стоит заменить на loadbuffer в будущем
     luaL_error(L, "Error running new Lua state: %s", lua_tostring(L1, -1));
   if (pthread_create(&thread, NULL, fio_thread, L1))
     luaL_error(L, "Can't create new thread");
+//  pthread_detach(thread);
+  lua_pushinteger(L, thread); // Возвращает идентификатор потока
+  return 1;
+}
+/*-------------------------------------------------------------*/
+static int fio_join(lua_State *L)
+{
+  pthread_t thread = lua_tointeger(L, 1);
+  pthread_join(thread, NULL);
+  return 0;
+}
+/*-------------------------------------------------------------*/
+static int fio_detach(lua_State *L)
+{
+  pthread_t thread = lua_tointeger(L, 1);
   pthread_detach(thread);
   return 0;
 }
 /*-------------------------------------------------------------*/
+static int fio_mutex_create(lua_State *L)
+{
+  return 0;
+}
+/*-------------------------------------------------------------*/
+static void fio_mutex_lock()
+{
+
+}
+/*-------------------------------------------------------------*/
+static void fio_mutex_unlock()
+{
+
+}
+/*-------------------------------------------------------------*/
+/* Если главный поток завершит работу, дочерние не перестанут выполняться */
 static int fio_exit()
 {
   pthread_exit(NULL);
@@ -71,19 +137,4 @@ static int l_dir(lua_State *L)
   return 1;
 }
 /*-------------------------------------------------------------*/
-static const struct luaL_Reg fio [] = {
-  {"start", fio_start},
-  {"exit", fio_exit},
-  {"dir", l_dir},
-  {NULL, NULL}
-};
-/*-------------------------------------------------------------*/
-int luaopen_fio(lua_State *L)
-{
-  Proc *self = (Proc *) lua_newuserdata(L, sizeof(Proc));
-  lua_setfield(L, LUA_REGISTRYINDEX, "_SELF");
-  self->L = L;
-  self->thread = pthread_self();
-  luaL_newlib(L, fio);
-  return 1;
-}
+
