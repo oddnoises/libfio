0b84972f11250b37244eb1359c64a00b522afcf5
oddnoises <choob1900@mail.ru>
2025-03-18 19:08:51 +0300

    Add init functions

--- src/hashtable.c
+++ src/hashtable.c
@@ -9,6 +9,50 @@ get_table_index(HashTable_s *table, void *key);
 static int ht_common_cmp_str(const void *str1, const void *str2);
 static __inline size_t round_pow_two(size_t n);
 /*-------------------------------------------------------------*/
+enum ht_stat ht_new(HashTable_s **out)
+{
+  HashTableConf htc;
+  ht_conf_init(&htc);
+  return ht_new_conf(&htc, out);
+}
+/*-------------------------------------------------------------*/
+enum ht_stat ht_new_conf(HashTableConf const *const conf, HashTable_s **out)
+{
+  HashTable_s *table = conf->mem_calloc(1, sizeof(HashTable_s));
+  if (!table)
+    return HT_ERR_ALLOC;
+  table->capacity = round_pow_two(conf->initial_capacity);
+  table->buckets = conf->mem_calloc(table->capacity, sizeof(TableEntry*));
+  if (!table->buckets) {
+    conf->mem_free(table);
+    return HT_ERR_ALLOC;
+  }
+  table->hash = conf->hash;
+  table->key_cmp = conf->key_compare;
+  table->load_factor = conf->load_factor;
+  table->hash_seed = conf->hash_seed;
+  table->key_len = conf->key_len;
+  table->size = 0;
+  table->mem_alloc = conf->mem_alloc;
+  table->mem_calloc = conf->mem_calloc;
+  table->mem_free = conf->mem_free;
+  table->threshold = (size_t) (table->capacity * table->load_factor);
+  *out = table;
+  return HT_OK;
+}
+/*-------------------------------------------------------------*/
+void ht_conf_init(HashTableConf *conf)
+{
+  conf->hash = string_hash;
+  conf->key_compare = ht_common_cmp_str;
+  conf->initial_capacity = DEFAULT_CAPACITY;
+  conf->key_len = KEY_LENGTH_VARIABLE;
+  conf->hash_seed = 0;
+  conf->mem_alloc = malloc;
+  conf->mem_calloc = calloc;
+  conf->mem_free = free;
+}
+/*-------------------------------------------------------------*/
 size_t string_hash(const void *key, int len, u_int32_t seed)
 {
   const char *str = key;
