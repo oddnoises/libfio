e0b9b27beb8cbb85a39034b166d75b31e06af273
oddnoises <choob1900@mail.ru>
2025-03-18 19:12:38 +0300

    Add ht_add() and ht_get() functions

--- src/hashtable.c
+++ src/hashtable.c
@@ -53,6 +53,121 @@ void ht_conf_init(HashTableConf *conf)
   conf->mem_free = free;
 }
 /*-------------------------------------------------------------*/
+enum ht_stat ht_add(HashTable_s *table, void *key, void *val)
+{
+  enum ht_stat res;
+  if (table->size >= table->threshold) {
+    if ((res = resize(table, table->capacity << 1)) != HT_OK)
+      return res;
+  }
+  if (!key)
+    return HT_ERR_NULL_VAL;
+  const size_t hash = table->hash(key, table->key_len, table->hash_seed);
+  const size_t index = hash & (table->capacity - 1);
+  TableEntry *replace = table->buckets[index];
+  while (replace) {
+    void *rk = replace->key;
+    if (rk && table->key_cmp(rk, key) == 0) {
+      replace->value = val;
+      return HT_OK;
+    }
+    replace = replace->next;
+  }
+  TableEntry *new_entry = table->mem_alloc(sizeof(TableEntry));
+  if (!new_entry)
+    return HT_ERR_ALLOC;
+  new_entry->key = key;
+  new_entry->value = val;
+  new_entry->hash = hash;
+  new_entry->next = table->buckets[index];
+  table->buckets[index] = new_entry;
+  table->size++;
+  return HT_OK;
+}
+/*-------------------------------------------------------------*/
+enum ht_stat ht_get(HashTable_s *table, void *key, void **out)
+{
+  if (!key)
+    return HT_ERR_NULL_VAL;
+  size_t index = get_table_index(table, key);
+  TableEntry *bucket = table->buckets[index];
+  while (bucket) {
+    if (bucket->key && table->key_cmp(bucket->key, key) == 0) {
+      *out = bucket->value;
+      return HT_OK;
+    }
+    bucket = bucket->next;
+  }
+  return HT_ERR_KEY_NOT_FOUND;
+}
+/*-------------------------------------------------------------*/
+enum ht_stat ht_remove(HashTable_s *table, void *key, void **out)
+{
+  if (!key)
+    return HT_ERR_NULL_VAL;
+  const size_t index = get_table_index(table, key);
+  TableEntry *entry = table->buckets[index];
+  TableEntry *prev = NULL, *next = NULL;
+  while (entry) {
+    next = entry->next;
+    if (entry->key && table->key_cmp(key, entry->key) == 0) {
+      void *value = entry->value;
+      if (!prev)
+        table->buckets[index] = next;
+      else
+        prev->next = next;
+      table->mem_free(entry);
+      table->size--;
+      if (out)
+        *out = value;
+      return HT_OK;
+    }
+    prev = entry;
+    entry = next;
+  }
+  return HT_ERR_KEY_NOT_FOUND;
+}
+/*-------------------------------------------------------------*/
+void ht_remove_all(HashTable_s *table)
+{
+  for (size_t i = 0; i < table->capacity; i++) {
+    TableEntry *entry = table->buckets[i];
+    while (entry) {
+      TableEntry *next = entry->next;
+      table->mem_free(entry);
+      table->size--;
+      entry = next;
+    }
+    table->buckets[i] = NULL;
+  }
+}
+/*-------------------------------------------------------------*/
+void ht_destroy(HashTable_s *table)
+{
+  for (size_t i = 0; i < table->capacity; i++) {
+    TableEntry *next = table->buckets[i];
+    while (next) {
+      TableEntry *tmp = next->next;
+      table->mem_free(next);
+      next = tmp;
+    }
+  }
+  table->mem_free(table->buckets);
+  table->mem_free(table);
+}
+/*-------------------------------------------------------------*/
+bool ht_contains_key(HashTable_s *table, void *key)
+{
+  size_t index = get_table_index(table, key);
+  TableEntry *entry = table->buckets[index];
+  while (entry) {
+    if (table->key_cmp(key, entry->key) == 0)
+      return true;
+    entry = entry->next;
+  }
+  return false;
+}
+/*-------------------------------------------------------------*/
 size_t string_hash(const void *key, int len, u_int32_t seed)
 {
   const char *str = key;
