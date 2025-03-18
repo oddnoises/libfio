debd6c05a2612f9788d09f8c44ffac48f0251f3b
oddnoises <choob1900@mail.ru>
2025-03-18 19:04:20 +0300

    Add basic funcs for hashtables

--- src/hashtable.c
+++ src/hashtable.c
@@ -1,7 +1,84 @@
 #include "hashtable.h"
-#include <stddef.h>
-#include <sys/types.h>
 
-#define DEFAULT_CAPACITY 16
-#define DEFAULT_LOAD_FACTOR 0.75f
+static enum ht_stat resize(HashTable_s *table, size_t new_capacity);
+static __inline void
+move_entries(TableEntry **src_bucket, TableEntry **dst_bucket,
+             size_t       src_size,   size_t       dst_size);
+static __inline size_t
+get_table_index(HashTable_s *table, void *key);
+static int ht_common_cmp_str(const void *str1, const void *str2);
+static __inline size_t round_pow_two(size_t n);
+/*-------------------------------------------------------------*/
+size_t string_hash(const void *key, int len, u_int32_t seed)
+{
+  const char *str = key;
+  int c;
+  register size_t hash = seed + 5381 + len + 1;
+  while ((c = *str++))
+    hash = ((hash << 5) + hash) ^ c;
+  return hash;
+}
+/*-------------------------------------------------------------*/
+static enum ht_stat resize(HashTable_s *table, size_t new_capacity)
+{
+  if (table->capacity >= MAX_POW_TWO)
+    return HT_ERR_MAX_CAPACITY;
+  TableEntry **new_buckets = table->mem_calloc(new_capacity, sizeof(TableEntry));
+  if (!new_buckets)
+    return HT_ERR_ALLOC;
+  TableEntry **old_buckets = table->buckets;
+  move_entries(old_buckets, new_buckets, table->capacity, new_capacity);
+  table->buckets = new_buckets;
+  table->capacity = new_capacity;
+  table->threshold = (size_t) (table->load_factor * new_capacity);
+  table->mem_free(old_buckets);
+  return HT_OK;
+}
+/*-------------------------------------------------------------*/
+static __inline size_t
+get_table_index(HashTable_s *table, void *key)
+{
+  size_t hash = string_hash(key, table->key_len, table->hash_seed);
+  return hash & (table->capacity - 1);
+}
+/*-------------------------------------------------------------*/
+static __inline void
+move_entries(TableEntry **src_bucket, TableEntry **dst_bucket,
+             size_t       src_size,   size_t       dst_size)
+{
+  for (size_t i = 0; i < src_size; i++) {
+    TableEntry *entry = src_bucket[i];
+
+    while (entry) {
+      TableEntry *next = entry->next;
+      size_t index = entry->hash & (dst_size - 1);
+
+      entry->next = dst_bucket[index];
+      dst_bucket[index] = entry;
+
+      entry = next;
+    }
+  }
+}
+/*-------------------------------------------------------------*/
+static int ht_common_cmp_str(const void *str1, const void *str2)
+{
+    return strcmp((const char*) str1, (const char*) str2);
+}
+/*-------------------------------------------------------------*/
+static __inline size_t round_pow_two(size_t n)
+{
+  if (n >= MAX_POW_TWO)
+    return MAX_POW_TWO;
+  if (!n)
+    return 1;
+  n--;
+  n |= n >> 1;
+  n |= n >> 2;
+  n |= n >> 4;
+  n |= n >> 8;
+  n |= n >> 16;
+  n++;
+  return n;
+}
 
--- src/hashtable.h
+++ src/hashtable.h
@@ -1,47 +1,60 @@
-#include <cstddef>
-#include <cstdint>
-#include <math.h>
-#include <stddef.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  void    *key;
-  void    *value;
-  size_t  *hash;
+#define MAX_POW_TWO (((size_t) 1) << 31)
+#define DEFAULT_CAPACITY 16
+#define DEFAULT_LOAD_FACTOR 0.75f
+#define KEY_LENGTH_VARIABLE  -1
+
+enum ht_stat {
+    HT_OK                   = 0,
+
+    HT_ERR_ALLOC            = 1,
+    HT_ERR_INVALID_CAPACITY = 2,
+    HT_ERR_INVALID_RANGE    = 3,
+    HT_ERR_MAX_CAPACITY     = 4,
+    HT_ERR_KEY_NOT_FOUND    = 6,
+    HT_ERR_VALUE_NOT_FOUND  = 7,
+    HT_ERR_OUT_OF_RANGE     = 8,
+    HT_ITER_END             = 9,
+    HT_ERR_NULL_VAL         = 10,
+    HT_ERR_SAMEKEY          = 11,
+};
+
+typedef struct table_entry_s {
+  void *key;
+  void *value;
+  size_t hash;
+  struct table_entry_s *next;
 } TableEntry;
 
 typedef struct {
   size_t    capacity;
   size_t    size;
   size_t    threshold;
-  uint32_t  hash_seed;
+  u_int32_t hash_seed;
   int       key_len;
   float     load_factor;
   TableEntry **buckets;
-} HashTable;
-/*-------------------------------------------------------------*/
-static size_t calc_hash(const void *key, int len, uint32_t seed)
-{
-  const char *str = key;
-  register size_t hash = seed + 5381 + len + 1;
-  int c;
-  while ((c = *str++))
-    hash = ((hash << 5) + hash) ^ c;
-  return hash;
-}
-/*-------------------------------------------------------------*/
-static int resize(HashTable *t, size_t new_capacity)
-{
 
-}
-/*-------------------------------------------------------------*/
-int hashtable_add(HashTable *table, void *key, void *val)
-{
-  int res;
-  if (table->size >= table->threshold) {
-    
-  }
-}
-/*-------------------------------------------------------------*/
+  size_t (*hash) (const void *key, int l, uint32_t seed);
+  int (*key_cmp) (const void *k1, const void *k2);
+  void *(*mem_alloc) (size_t size);
+  void *(*mem_calloc) (size_t blocks, size_t size);
+  void (*mem_free) (void *block);
+} HashTable_s;
+
+typedef struct HashTableConf_s {
+  float load_factor;
+  size_t initial_capacity;
+  int key_len;
+  uint32_t hash_seed;
+  size_t (*hash) (const void *key, int l, uint32_t seed);
+  int (*key_compare) (const void *key1, const void *key2);
+  void *(*mem_alloc) (size_t size);
+  void *(*mem_calloc) (size_t blocks, size_t size);
+  void (*mem_free) (void *block);
+} HashTableConf;
