96b6532099329d2b0b83927bd537343a21eac81d
oddnoises <choob1900@mail.ru>
2025-03-20 21:10:31 +0300

    Implemented function fio_mutex_create

--- src/fio.c
+++ src/fio.c
@@ -1,12 +1,13 @@
-//#include <bits/pthreadtypes.h>
 #include <dirent.h>
 #include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <lua5.4/lauxlib.h>
 #include <lua5.4/lua.h>
 #include <lua5.4/lualib.h>
+#include "hashtable.h"
 
 static int luaopen_fio(lua_State *L);
 static void *fio_thread(void *arg);
@@ -18,20 +19,22 @@ static void fio_mutex_lock();
 static void fio_mutex_unlock();
 static int fio_exit();
 static int l_dir(lua_State *L);
-int luaopen_fio(lua_State *L);
+static void init_table();
 
-//static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-typedef struct Proc {
+typedef struct {
   lua_State *L;
   pthread_t thread;
 } Proc;
 
-typedef struct Mtex {
-  pthread_mutex_t *mtex;
+typedef struct {
+  pthread_mutex_t mutex;
   char *name;
   int users;
-} Mtex;
+} Mutex_s;
+
+pthread_once_t once_variable = PTHREAD_ONCE_INIT;
+static HashTable_s *MutexTable;
+
 /*-------------------------------------------------------------*/
 static const struct luaL_Reg fio [] = {
   {"start", fio_start},
@@ -97,8 +100,37 @@ static int fio_detach(lua_State *L)
 /*-------------------------------------------------------------*/
 static int fio_mutex_create(lua_State *L)
 {
-  pthread_mutex_t mutex;
+  Mutex_s *mutex_struct;
   const char *mutex_name = luaL_checkstring(L, 1);
+  int res;
+  res = pthread_once(&once_variable, init_table);
+  if (res)
+    luaL_error(L, "Error in pthread_once return %d value: %s", res, strerror(errno));
+  res = ht_contains_key(MutexTable, (void*) mutex_name);
+  if (res)
+    luaL_error(L, "Mutex with name [%s] is already exists!", mutex_name);
+  mutex_struct = malloc(sizeof(Mutex_s));
+  if (!mutex_struct)
+    luaL_error(L, "Error allocate memory for mutex struct: %s", strerror(errno));
+  res = pthread_mutex_init(&mutex_struct->mutex, NULL);
+  if (res)
+    luaL_error(L, "Error init new mutex: %s", strerror(errno));
+  mutex_struct->name = strdup(mutex_name);
+  res = ht_add(MutexTable, (void*) mutex_name, mutex_struct);
+  if (res)
+    luaL_error(L, "Can't add new mutex to global table: %d", res);
+  return 0;
+}
+/*-------------------------------------------------------------*/
+static int fio_mutex_destroy(lua_State *L)
+{
+  Mutex_s *mutex_struct;
+  const char *mutex_name = luaL_checkstring(L, 1);
+  int res;
+  res = ht_get(MutexTable, (void*) mutex_name, (void*) &mutex_struct);
+  if (res)
+    luaL_error(L, "Error getting mutex [%s] from table: %d", mutex_name, res);
+
   return 0;
 }
 /*-------------------------------------------------------------*/
@@ -139,6 +171,11 @@ static int l_dir(lua_State *L)
   }
   closedir(dir);
   return 1;
+}
+/*-------------------------------------------------------------*/
+static void init_table()
+{
+  ht_new(&MutexTable);
 }
 /*-------------------------------------------------------------*/
 
