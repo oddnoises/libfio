bcfac532b8e2d8a00c049f57ea5935d48ca2e6a2
oddnoises <choob1900@mail.ru>
2025-03-20 19:13:49 +0300

    Fix nob file

--- nob.c
+++ nob.c
@@ -16,20 +16,20 @@ int main(int argc, char **argv)
   if (!nob_mkdir_if_not_exists(BUILD_FOLFER)) return 1;
   if (!nob_mkdir_if_not_exists(OBJ_FOLDER)) return 1;
 
+  nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra", "-fPIC", "-c");
+  nob_cmd_append(&cmd, SRC_FOLDER"hashtable.c", "-o", OBJ_FOLDER"hashtable.o");
+  if (!nob_cmd_run_sync(cmd)) return 1;
+
+  cmd.count = 0;
   nob_cmd_append(&cmd, "gcc", "-pthread");
   nob_cmd_append(&cmd, "-Wall", "-Wextra", "-fPIC", "-c");
   nob_cmd_append(&cmd, SRC_FOLDER"fio.c", "-o", OBJ_FOLDER"fio.o");
   if (!nob_cmd_run_sync(cmd)) return 1;
 
-//  cmd.count = 0;
-//  nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra", "-fPIC", "-c");
-//  nob_cmd_append(&cmd, SRC_FOLDER"hashtable.c", "-o", OBJ_FOLDER"hashtable.o");
-//  if (!nob_cmd_run_sync(cmd)) return 1;
-
   cmd.count = 0;
   nob_cmd_append(&cmd, "gcc", "-pthread");
   nob_cmd_append(&cmd, "-Wall", "-Wextra", "-shared");
-  nob_cmd_append(&cmd, "-o", BUILD_FOLFER"fio.so", OBJ_FOLDER"fio.o");
+  nob_cmd_append(&cmd, "-o", BUILD_FOLFER"fio.so", OBJ_FOLDER"fio.o", OBJ_FOLDER"hashtable.o");
   if (!nob_cmd_run_sync(cmd)) return 1;
   return 0;
 }
